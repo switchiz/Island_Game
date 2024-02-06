52619116a0d3b445b18f02d562e1bd4130679ebe
switchiz <starter128@gmail.com>
2024-02-06 14:21:00 +0900

    4_ 플레이어 마우스 클릭 / 인풋액션
    플레이어가 클릭한 좌표로 이동함.

--- Assets/Script/System/Player.cs
+++ Assets/Script/System/Player.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    PlayerInput playerinput;
+
     /// <summary>
     /// 시작 위치 ( x,z )
     /// </summary>
@@ -11,15 +15,63 @@ public class Player : MonoBehaviour
 
     Vector3 Player_pos;
 
+    private void Awake()
+    {
+        playerinput = new();
+    }
+
+    private void OnEnable()
+    {
+        playerinput.Player.Enable();
+        playerinput.Player.Click.performed += OnClcik;
+    }
+
+
+    private void OnDisable()
+    {
+        playerinput.Player.Click.performed -= OnClcik;
+        playerinput.Player.Disable();
+    }
+
+
+
     private void Start()
     {
         transform.position = new Vector3(x*0.4f, 0.4f, z*0.4f);
         Player_pos = transform.position;
+        alignPlayer();
     }
 
     private void Update()
     {
-        alignPlayer();
+        
+    }
+
+
+    /// <summary>
+    /// 클릭의 위치를 주는 메서드
+    /// </summary>
+    /// <param name="context"></param>
+    private void OnClcik(UnityEngine.InputSystem.InputAction.CallbackContext context)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            MovePlayerTo(hit.point);
+            
+        }
+    }
+
+
+    /// <summary>
+    /// 클릭한곳으로 이동시키는 메서드
+    /// </summary>
+    private void MovePlayerTo(Vector3 point)
+    {
+        Debug.Log(point);
+        transform.position = point;
     }
 
     private void alignPlayer()
@@ -31,7 +83,7 @@ public class Player : MonoBehaviour
         if (Physics.Raycast(ray, out RaycastHit hitInfo, 2.0f) )
         {
             Debug.Log(hitInfo.collider);
-             if ( hitInfo.collider == GameObject.FindGameObjectWithTag("Floor_1") )
+             if (hitInfo.collider.gameObject.CompareTag("Floor_1"))
             {
                 transform.position = new Vector3(transform.position.x, 0.4f, transform.position.z);
             }
